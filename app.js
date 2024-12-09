d1b2028399bd90d3e9d47cedb4f904821d65cd77
PauloRaphael <Paulo.Raphael.Web@gmail.com>
2024-12-09 13:32:38 -0300

    Third lesson: Functions with parameters

--- app.js
+++ app.js
@@ -1,10 +1,19 @@
 
-let titulo = document.querySelector('h1');
-titulo.innerHTML = 'Jogo do número secreto';
+// let titulo = document.querySelector('h1');
+// titulo.innerHTML = 'Jogo do número secreto';
 
-let paragrafo = document.querySelector('p');
-paragrafo.innerHTML = 'Escolha um número entre 1 e 10';
+// let paragrafo = document.querySelector('p');
+// paragrafo.innerHTML = 'Escolha um número entre 1 e 10';
+
+function exibirTextoNaTela(tag, texto) {
+    let campo = documento.querySelector(tag);
+    campo.innerHTML = texto;
+}
+
+exibirTextoNaTela('h1', 'Jogo do número secreto')
+exibirTextoNaTela('p', 'Escolha um número entre 1 e 10')
 
 function verificarChute() {
     console.log('Um botão foi clicado');
 }
+
