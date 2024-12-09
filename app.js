b362dc28dbe648912e8294dc44ff4ed3aa44b937
PauloRaphael <Paulo.Raphael.Web@gmail.com>
2024-12-09 14:47:31 -0300

    Tenth lesson: Lists and recursion

--- app.js
+++ app.js
@@ -5,6 +5,7 @@
 // let paragrafo = document.querySelector('p');
 // paragrafo.innerHTML = 'Escolha um número entre 1 e 10';
 
+let listaDeNumerosSorteados = [];
 let numeroSecreto = gerarNumeroAleatorio();
 let tentativas = 1;
 
@@ -41,7 +42,14 @@ function verificarChute() {
 }
 
 function gerarNumeroAleatorio() {
-    return parseInt(Math.random() * 10 + 1);
+    let numeroEscolhido = parseInt(Math.random() * 10 + 1);
+    
+    if(listaDeNumerosSorteados.includes(numeroEscolhido)) {
+        gerarNumeroAleatorio();
+    } else {
+        listaDeNumerosSorteados.push(numeroEscolhido);
+        return numeroEscolhido;
+    }
 }
 
 function limparCampo() {
